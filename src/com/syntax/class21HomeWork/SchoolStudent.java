87e304c90b57e3c31c1cd5b0390b4d2ba1557ae9
“hemmat9” <farhad.hemmat9@gmail.com>
2021-11-23 15:25:20 -0800

    OPPs class and HomeWorks

---
+++ src/com/syntax/class21HomeWork/SchoolStudent.java
@@ -0,0 +1,12 @@
+package com.syntax.class21HomeWork;
+
+public class SchoolStudent extends Student{
+
+    void studyHard(){
+        System.out.println("don't study that much, but plays a lot");
+    }
+    void play(){
+        System.out.println(" play lots of video games");
+    }
+
+}
---
+++ src/com/syntax/class21HomeWork/SyntaxStudent.java
@@ -0,0 +1,14 @@
+package com.syntax.class21HomeWork;
+
+public class SyntaxStudent extends Student {
+
+    void studyHard(){
+        System.out.println("students always study Hard and practice more");
+    }
+    void sleep(){
+        System.out.println("thinking about coding before they sleep");
+    }
+    void vocation(){
+        System.out.println("no vocation for syntax Students for six months");
+    }
+}
